9ed0d0cc7373467164063051e7cd882b646083ee
Виктор Щеглов <valeshcheglov@sberbank.ru>
2024-12-10 22:04:45 +0300

    LAB1: сделал исправления по первому ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,23 @@
-function createCard(cardParams) {
-    const card = document.querySelector('#card-template')
-        .content
-        .querySelector('.card')
-        .cloneNode(true);
-    let cardDeleteButton = card.querySelector('.card__delete-button');
-    cardDeleteButton.addEventListener('click', (event) => {
-        event.currentTarget.targetElement.remove()
-    });
+const cardTemplate = document.querySelector('#card-template')
+const placesList = document.querySelector('.places__list')
+
+function createCard(cardParams, deleteEvent) {
+    const card = cardTemplate.content.querySelector('.card').cloneNode(true)
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    const cardImage = card.querySelector(".card__image")
+
+    cardDeleteButton.addEventListener('click', deleteEvent);
     cardDeleteButton.targetElement = card;
     card.querySelector('.card__title').textContent = cardParams.name;
-    card.querySelector('.card__image').setAttribute('src', cardParams.link);
+    cardImage.src = cardParams.link
+    cardImage.alt = cardParams.link
     return card;
 }
 
+function deleteCard(event) {
+    event.currentTarget.targetElement.remove();
+}
+
 initialCards.forEach((cardParams) => {
-    document.querySelector('.places__list').append(createCard(cardParams));
+    placesList.append(createCard(cardParams, deleteCard));
 });
